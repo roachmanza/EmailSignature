ba551e603f078a7fc74338c789cdeeb853eb4cd5
roachmanza <roche.olivier@outlook.com>
2017-09-19 17:55:28 +0200

    Completed CotactType API

--- Api_Site/api/controllers/ContactTypesController.js
+++ Api_Site/api/controllers/ContactTypesController.js
@@ -14,15 +14,15 @@ exports.create_a_ContactType = function (req, res) {
             if (numberResults[0] != null) {
                 id = numberResults[0]["ContactTypeId"] + 1;
             }
-            var ascriptionId = req.body.ascriptionId;
-            var fromAscriptionStatusId = req.body.fromAscriptionStatusId;
-            var toAscriptionStatusId = req.body.toAscriptionStatusId;
-            var details = req.body.details;
-            var createDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+            var name = req.body.name;
+            var description = req.body.description;
+            var email = req.body.contactTypeEmailAddress;
+            var inactiveDate = new Date(1900, 01,01).toJSON().slice(0, 10).replace(/-/g, '/');
+            var inactive = 0;
             var dataPost = require('../dataAccess/dataPost');
-            dataPost('INSERT INTO public."ContactTypes"("ContactTypeId", "AscriptionId", "FromAscriptionStatusId", "ToAscriptionStatusId","Details", "CreateDate") ' +
+            dataPost('INSERT INTO public."ContactTypes"("ContactTypeId", "Name", "Description", "ContactTypeEmailAddress","InActiveDate", "InActive") ' +
                 'VALUES' +
-                '(' + id + ',\'' + ascriptionId + '\' ,\'' + fromAscriptionStatusId + '\' ,\'' + toAscriptionStatusId + '\' ,\'' + details + '\' ,\'' + createDate + '\')',
+                '(' + id + ',\'' + name + '\' ,\'' + description + '\' ,\'' + email + '\' ,\'' + inactiveDate + '\' ,\'' + inactive + '\')',
                 function (results) {
                     res.send(results);
                 });
@@ -40,17 +40,28 @@ exports.read_a_ContactType = function (req, res) {
 
 exports.update_a_ContactType = function (req, res) {
     var id = req.params.ContactTypeId;
-    var ascriptionId = req.body.ascriptionId;
-    var fromAscriptionStatusId = req.body.fromAscriptionStatusId;
-    var toAscriptionStatusId = req.body.toAscriptionStatusId;
-    var details = req.body.details;
+    var name = req.body.name;
+    var description = req.body.description;
+    var email = req.body.contactTypeEmailAddress;
+    var inactiveDate;
+    var inactive;
+    if(req.body.inActive==="1"){
+        inactive = 1;
+        inactiveDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+    }else{
+        inactive = 0;
+        inactiveDate = new Date(1900, 01,01).toJSON().slice(0, 10).replace(/-/g, '/');
+    }
+   
+    
     var dataPut = require('../dataAccess/dataPut');
     dataPut(' UPDATE public."ContactTypes" ' +
         'SET ' +
-        ' "AscriptionId"=\'' + ascriptionId + '\', ' +
-        ' "FromAscriptionStatusId"=\'' + fromAscriptionStatusId + '\', ' +
-        ' "ToAscriptionStatusId"=\'' + toAscriptionStatusId + '\', ' +
-        ' "Details"=\'' + details + '\' ' +
+        ' "Name"=\'' + name + '\', ' +
+        ' "Description"=\'' + description + '\', ' +
+        ' "ContactTypeEmailAddress"=\'' + email + '\', ' +
+        ' "InActiveDate"=\'' + inactiveDate + '\', ' +
+        ' "InActive"=\'' + inactive + '\' ' +
         'where "ContactTypeId" = ' + id,
         function (results) {
             res.send(results);
