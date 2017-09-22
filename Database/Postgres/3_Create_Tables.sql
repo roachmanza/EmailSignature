4288fe0b5e6ae75b97456ec0b681464242ec76af
roachmanza <roche.olivier@outlook.com>
2017-09-22 22:05:40 +0200

    Updated ERD with new design

--- Database/Postgres/3_Create_Tables.sql
+++ Database/Postgres/3_Create_Tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE public."ContactTypes"
     "ContactTypeId" integer NOT NULL,
     "Name" character varying COLLATE pg_catalog."default",
     "Description" character varying COLLATE pg_catalog."default",
-    "ContactTypeEmailAddress" character varying COLLATE pg_catalog."default",
+    "EmailAddress" character varying COLLATE pg_catalog."default",
     "InActiveDate" date,
     "InActive" bit(1),
     CONSTRAINT "ContactTypes_pkey" PRIMARY KEY ("ContactTypeId")
@@ -71,9 +71,9 @@ TABLESPACE pg_default;
 ALTER TABLE public."CsiContactCategories" OWNER TO "MailEnhancementUser";
 
 --|| NO 1.5 ||--
-CREATE TABLE public."CsiContactTypes"
+CREATE TABLE public."CsiMainContactTypes"
 (
-    "CsiContactTypeId" integer NOT NULL,
+    "CsiMainContactTypeId" integer NOT NULL,
     "Name" character varying COLLATE pg_catalog."default",
     "Description" character varying COLLATE pg_catalog."default",
     "InActiveDate" date,
@@ -84,10 +84,62 @@ WITH (
     OIDS = FALSE
 )
 TABLESPACE pg_default;
+ALTER TABLE public."CsiMainContactTypes" OWNER TO "MailEnhancementUser";
+
+--||=================||--
+--|| SECOND SEQUENCE ||--
+--||=================||--
+--|| NO 2.1 ||--
+CREATE TABLE public."CsiContactCategoryMappings"
+(
+    "CsiContactCategoryMappingId" integer NOT NULL,
+    "CsiContactCategoryId" integer NOT NULL,
+    "ContactTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "CsiContactType_pkey" PRIMARY KEY ("CsiContactTypeId"),
+    CONSTRAINT "CsiContactCategoryMappings_CsiContactCategories_fkey" FOREIGN KEY ("CsiContactCategoryId")
+        REFERENCES public."CsiContactCategories" ("CsiContactCategoryId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "CsiContactCategoryMappings_ContactTypes_fkey" FOREIGN KEY ("ContactTypeId")
+        REFERENCES public."ContactTypes" ("ContactTypeId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+ALTER TABLE public."CsiContactCategoryMappings" OWNER TO "MailEnhancementUser";
+
+
+--|| NO 2.2 ||--
+CREATE TABLE public."CsiContactTypes"
+(
+    "CsiContactTypeId" integer NOT NULL,
+    "CsiMainContactTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "CsiContactType_pkey" PRIMARY KEY ("CsiContactTypeId"),
+    CONSTRAINT "CsiContactTypes_CsiMainContactTypes_fkey" FOREIGN KEY ("CsiMainContactTypeId")
+        REFERENCES public."CsiMainContactTypes" ("CsiMainContactTypeId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 ALTER TABLE public."CsiContactTypes" OWNER TO "MailEnhancementUser";
 
 
---|| NO 1.6 ||--
+
+--|| NO 2.3 ||--
 CREATE TABLE public."AwdContactTypeMappings"
 (
     "AwdContactTypeMappingId" integer NOT NULL,
@@ -110,23 +162,66 @@ WITH (
 TABLESPACE pg_default;
 ALTER TABLE public."AwdContactTypeMappings" OWNER TO "MailEnhancementUser";
 
---||===============||--
---|| Signatures    ||--
---||===============||--
---|| NO 2.1 ||--
+--|| NO 2.4 ||--
+CREATE TABLE public."CsiContactTypeMappings"
+(
+    "CsiContactTypeMappingId" integer NOT NULL,
+    "ContactTypeId" integer NOT NULL,
+    "AwdRegion" character varying COLLATE pg_catalog."default",
+    "AwdContactRole" character varying COLLATE pg_catalog."default",
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "CsiContactTypeMappings_pkey" PRIMARY KEY ("CsiContactTypeMappingId"),
+    CONSTRAINT "CsiContactTypeMappings_ContactTypes_fkey" FOREIGN KEY ("ContactTypeId")
+        REFERENCES public."ContactTypes" ("ContactTypeId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+ALTER TABLE public."CsiContactTypeMappings" OWNER TO "MailEnhancementUser";
+
+
+--|| NO 2.5 ||--
+CREATE TABLE public."FieldItems"
+(
+    "FieldItemId" integer NOT NULL,
+    "LanguageId" integer NOT NULL,
+    "FieldTypeId" integer NOT NULL,
+    "Name" character varying COLLATE pg_catalog."default",
+    "Description" character varying COLLATE pg_catalog."default",
+    "Label" character varying COLLATE pg_catalog."default",
+    "Value" character varying COLLATE pg_catalog."default",
+    "PrintFormat" character varying COLLATE pg_catalog."default",    
+    "InActiveDate" date,
+    "InActive" bit(1),
+    CONSTRAINT "FieldItems_pkey" PRIMARY KEY ("FieldItemId"),
+    CONSTRAINT "FieldItems_Languages_fkey" FOREIGN KEY ("LanguageId")
+        REFERENCES public."Languages" ("LanguageId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT "FieldItems_FieldTypes_fkey" FOREIGN KEY ("FieldTypeId")
+        REFERENCES public."FieldTypes" ("FieldTypeId") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+ALTER TABLE public."FieldItems" OWNER TO "MailEnhancementUser";
+
+--|| NO 2.6 ||--
 CREATE TABLE public."SignatureItems"
 (
     "SignatureItemId" integer NOT NULL,
     "ContactTypeId" integer NOT NULL,
-    "FieldTypeId" integer NOT NULL,
-    "LanguageId" integer NOT NULL,
-    "CsiContactCategoryId" integer NOT NULL,
-    "CsiContactTypeId" integer NOT NULL,
-    "CsiMainContactTypeId" integer NOT NULL,
+    "FieldItemId" integer NOT NULL,
     "Sequence" integer NOT NULL,
-    "Label" character varying COLLATE pg_catalog."default",
-    "Value" character varying COLLATE pg_catalog."default",     
-    "PrintFormat" character varying COLLATE pg_catalog."default",    
     "InActiveDate" date,
     "InActive" bit(1),
     CONSTRAINT "SignatureItems_pkey" PRIMARY KEY ("SignatureItemId"),
@@ -134,24 +229,8 @@ CREATE TABLE public."SignatureItems"
         REFERENCES public."ContactTypes" ("ContactTypeId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
-    CONSTRAINT "SignatureItems_FieldTypes_fkey" FOREIGN KEY ("FieldTypeId")
-        REFERENCES public."FieldTypes" ("FieldTypeId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "SignatureItems_Languages_fkey" FOREIGN KEY ("LanguageId")
-        REFERENCES public."Languages" ("LanguageId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "SignatureItems_CsiContactCategories_fkey" FOREIGN KEY ("CsiContactCategoryId")
-        REFERENCES public."CsiContactCategories" ("CsiContactCategoryId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "SignatureItems_CsiContactTypes_fkey" FOREIGN KEY ("CsiContactTypeId")
-        REFERENCES public."CsiContactTypes" ("CsiContactTypeId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "SignatureItems_CsiMainContactTypes_fkey" FOREIGN KEY ("CsiMainContactTypeId")
-        REFERENCES public."CsiContactTypes" ("CsiContactTypeId") MATCH SIMPLE
+    CONSTRAINT "SignatureItems_FieldItems_fkey" FOREIGN KEY ("FieldItemId")
+        REFERENCES public."FieldItems" ("FieldItemId") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
