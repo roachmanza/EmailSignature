e3e7eacbc6c13d97813003c80dbbf2cbf8404c7d
Roche Olivier <ROlivier@mmiholdings.co.za>
2017-10-09 11:48:42 +0200

    Updated the dataaccess

--- Api_Site/api/dataAccess/bll/SignatureItemsBll.js
+++ Api_Site/api/dataAccess/bll/SignatureItemsBll.js
@@ -1,19 +1,88 @@
-exports.TableSelectAllSignatureItems = 'SELECT ' +
-        ' SI."SignatureItemId" as SignatureItemId, ' +
-        ' SI."ContactTypeId" as ContactTypeId, ' +
-        ' CT."Name" as ContactTypeIdString, ' +
-        ' SI."FieldItemId" as FieldItemId, ' +
-        ' FI."Name" as FieldItemIdString, ' +
-        ' SI."Sequence" as Sequence, ' +
-        ' SI."InActiveDate" as InActiveDate, ' +
-        ' SI."InActive" as InActive ' +
-        ' FROM public."SignatureItems" AS SI' +
-        ' JOIN public."ContactTypes" as CT ON CT."ContactTypeId" = SI."ContactTypeId" ' +
-        ' JOIN public."FieldItems" as FI ON FI."FieldItemId" = SI."FieldItemId" ';
+exports.TableSelect = 'SELECT ' +
+    ' SI."SignatureItemId" as SignatureItemId, ' +
+    ' SI."ContactTypeId" as ContactTypeId, ' +
+    ' CT."Name" as ContactTypeIdString, ' +
+    ' SI."FieldItemId" as FieldItemId, ' +
+    ' FI."Name" as FieldItemIdString, ' +
+    ' SI."Sequence" as Sequence, ' +
+    ' SI."InActiveDate" as InActiveDate, ' +
+    ' SI."InActive" as InActive ' +
+    ' FROM public."SignatureItems" AS SI' +
+    ' JOIN public."ContactTypes" as CT ON CT."ContactTypeId" = SI."ContactTypeId" ' +
+    ' JOIN public."FieldItems" as FI ON FI."FieldItemId" = SI."FieldItemId" ';
+
+exports.TableCount = 'SELECT "SignatureItemId" FROM public."SignatureItems" order by "SignatureItemId" desc LIMIT 1';
+
+exports.TableInsert = 'INSERT INTO public."SignatureItems"' +
+    '("SignatureItemId", "ContactTypeId", "FieldItemId", "Sequence", "InActiveDate", "InActive") VALUES ';
+    
+exports.TableDelete = 'DELETE FROM public."SignatureItems" where "SignatureItemId" = ';
+    
+
+
+exports.create_a_SignatureItem = function (req, res) {
+    var dataGet = require('../dataAccess/dataGet');
+    var dataPost = require('../dataAccess/dataPost');
+    var countQueryString = exports.TableCount;
+    dataGet(countQueryString,
+        function (numberResults) {
+            var id = 1;
+            if (numberResults[0] != null) {
+                id = numberResults[0]["SignatureItemId"] + 1;
+            }
+            var contactTypeId = req.body.contactTypeId;
+            var fieldItemId = req.body.fieldItemId;
+            var sequence = req.body.sequence;
+            var inactiveDate = new Date(1900, 01, 01).toJSON().slice(0, 10).replace(/-/g, '/');
+            var inactive = 0;
+            var createQueryString = this.TableInsert +
+                '(' + id + ',\'' + contactTypeId + '\' ,\'' + fieldItemId + '\' ,\'' + sequence + '\' ,\'' + inactiveDate + '\' ,\'' + inactive + '\')';
+            dataPost(createQueryString, function (jsonResults, haserror, code) {
+                callback(jsonResults, haserror, code);
+            });
+        });
+};
+
+exports.update_a_SignatureItem = function (req, res) {
+    var dataPut = require('../dataAccess/dataPut');
+    var id = req.params.SignatureItemId;
+    var contactTypeId = req.body.contactTypeId;
+    var fieldItemId = req.body.fieldItemId;
+    var sequence = req.body.sequence;
+    var inactiveDate;
+    var inactive;
+    if (req.body.inActive === "1") {
+        inactive = 1;
+        inactiveDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+    } else {
+        inactive = 0;
+        inactiveDate = new Date(1900, 01, 01).toJSON().slice(0, 10).replace(/-/g, '/');
+    };   
+    dataPut(' UPDATE public."SignatureItems" ' +
+        'SET ' +
+        ' "ContactTypeId"=\'' + contactTypeId + '\', ' +
+        ' "FieldItemId"=\'' + fieldItemId + '\', ' +
+        ' "Sequence"=\'' + sequence + '\', ' +
+        ' "InActiveDate"=\'' + inactiveDate + '\', ' +
+        ' "InActive"=\'' + inactive + '\' ' +
+        'where "SignatureItemId" = ' + id,
+        function (jsonResults, haserror, code) {
+            callback(jsonResults, haserror, code);
+        });
+};
+
+exports.delete_a_SignatureItem = function (req, res) {
+    var id = req.params.SignatureItemId;
+    var dataDelete = require('../dataAccess/dataDelete');
+    dataDelete(this.TableDelete + id,
+        function (jsonResults, haserror, code) {
+            callback(jsonResults, haserror, code);
+        });
+};
 
 exports.get_all_SignatureItems = function (req, callback) {
     var dataGet = require('../dataAccess/dataGet');
-    var baseQuery = this.TableSelectAllSignatureItems + ' ORDER BY CT."Name" ASC , SI."Sequence" ASC, FI."Name" ASC';
+    var baseQuery = this.TableSelect + ' ORDER BY CT."Name" ASC , SI."Sequence" ASC, FI."Name" ASC';
     if (typeof req.query !== 'undefined' && req.query) {
         var sqlOrder = req.query.order; //console.log("There is a query string: ?order=" + sqlOrder);
         if (typeof sqlOrder !== 'undefined' && sqlOrder) { //There is a query string
@@ -32,88 +101,13 @@ exports.get_all_SignatureItems = function (req, callback) {
     }
 };
 
-exports.create_a_SignatureItem = function (req, res) {
-    var dataGet = require('../dataAccess/dataGet');
-    var dataPost = require('../dataAccess/dataPost');
-    var countQuery = 'SELECT "SignatureItemId" FROM public."SignatureItems" order by "SignatureItemId" desc LIMIT 1';
-    dataGet(countQuery,
-        function (numberResults) {
-            var id = 1;
-            if (numberResults[0] != null) {
-                id = numberResults[0]["SignatureItemId"] + 1;
-            }
-            var contactTypeId = req.body.contactTypeId;
-            var fieldItemId = req.body.fieldItemId;
-            var sequence = req.body.sequence;
-            var inactiveDate = new Date(1900, 01, 01).toJSON().slice(0, 10).replace(/-/g, '/');
-            var inactive = 0;
-            var createQuery = 'INSERT INTO public."SignatureItems"("SignatureItemId", "ContactTypeId", "FieldItemId", "Sequence", "InActiveDate", "InActive") VALUES ' +
-                '(' + id + ',\'' + contactTypeId + '\' ,\'' + fieldItemId + '\' ,\'' + sequence + '\' ,\'' + inactiveDate + '\' ,\'' + inactive + '\')';
-            dataPost(createQuery, function (jsonResults, haserror, code) {
-                callback(jsonResults, haserror, code);
-            });
-        });
-};
-
 exports.read_a_SignatureItem = function (req, res) {
     var id = req.params.SignatureItemId;
     var dataGet = require('../dataAccess/dataGet');
-    dataGet(
-
-        'SELECT ' +
-        ' SI."SignatureItemId" as SignatureItemId, ' +
-        ' SI."ContactTypeId" as ContactTypeId, ' +
-        ' CT."Name" as ContactTypeIdString, ' +
-        ' SI."FieldItemId" as FieldItemId, ' +
-        ' FI."Name" as FieldItemIdString, ' +
-        ' SI."Sequence" as Sequence, ' +
-        ' SI."InActiveDate" as InActiveDate, ' +
-        ' SI."InActive" as InActive ' +
-        ' FROM public."SignatureItems" AS SI' +
-        ' JOIN public."ContactTypes" as CT ON CT."ContactTypeId" = SI."ContactTypeId" ' +
-        ' JOIN public."FieldItems" as FI ON FI."FieldItemId" = SI."FieldItemId" ' +
-        ' AND SI."SignatureItemId" = ' + id,
-        function (jsonResults, haserror, code) {
-            callback(jsonResults, haserror, code);
-        });
-};
-
-exports.update_a_SignatureItem = function (req, res) {
-    var id = req.params.SignatureItemId;
-    var contactTypeId = req.body.contactTypeId;
-    var fieldItemId = req.body.fieldItemId;
-    var sequence = req.body.sequence;
-    var inactiveDate;
-    var inactive;
-    if (req.body.inActive === "1") {
-        inactive = 1;
-        inactiveDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
-    } else {
-        inactive = 0;
-        inactiveDate = new Date(1900, 01, 01).toJSON().slice(0, 10).replace(/-/g, '/');
-    }
-
-    var dataPut = require('../dataAccess/dataPut');
-    dataPut(' UPDATE public."SignatureItems" ' +
-        'SET ' +
-        ' "ContactTypeId"=\'' + contactTypeId + '\', ' +
-        ' "FieldItemId"=\'' + fieldItemId + '\', ' +
-        ' "Sequence"=\'' + sequence + '\', ' +
-        ' "InActiveDate"=\'' + inactiveDate + '\', ' +
-        ' "InActive"=\'' + inactive + '\' ' +
-        'where "SignatureItemId" = ' + id,
-        function (jsonResults, haserror, code) {
-            callback(jsonResults, haserror, code);
-        });
-};
-
-exports.delete_a_SignatureItem = function (req, res) {
-    var id = req.params.SignatureItemId;
-    var dataDelete = require('../dataAccess/dataDelete');
-    dataDelete('DELETE FROM public."SignatureItems" where "SignatureItemId" = ' + id,
-        function (jsonResults, haserror, code) {
-            callback(jsonResults, haserror, code);
-        });
+    var querystring = this.TableSelect + ' AND SI."SignatureItemId" = ' + id;
+    dataGet(querystring, function (jsonResults, haserror, code) {
+        callback(jsonResults, haserror, code);
+    });
 };
 
 exports.read_all_SignatureItems_for_emailAddr = function (req, res) {
--- Api_Site/api/dataAccess/data/dataDelete.js
+++ Api_Site/api/dataAccess/data/dataDelete.js
@@ -1,49 +1,34 @@
 var pg = require('pg');
 var config = require('config');
+var _httpresponcecode = require('../../constants/httpresponcecodes');
 
 module.exports = function (queryString, callback) {
     var connection;
+    var jsonReturnResult = "";
     if (config.has('serverSettings.postgresDbConnection')) {
         connection = config.get('serverSettings.postgresDbConnection');
+        var client = new pg.Client(connection);
+        client.connect(function (err) {
+            if (err) {
+                jsonReturnResult = JSON.parse(JSON.stringify(err.message));;
+                client.end();
+                callback(jsonReturnResult, true, _httpresponcecode.DBConnection_error); //Throws a 503
+            } else {
+                client.query(queryString, function (err, result) {
+                    if (err) {
+                        jsonReturnResult = JSON.parse(JSON.stringify(err.message));
+                        client.end();
+                        callback(jsonReturnResult, true, _httpresponcecode.Sql_error); //Throws a 400
+                    } else {
+                        jsonReturnResult = JSON.parse(JSON.stringify(result.rows));
+                        client.end();
+                        callback(jsonReturnResult, false, _httpresponcecode.Ok); //Throws a 200
+                    }
+                });
+            }
+        });
     } else {
-        var errorstring = "Connection error, could not find the 'serverSettings.postgresDbConnection' file."
-        callback(errorstring, true);
+        callback(JSON.parse(JSON.stringify("Connection error, could not find the 'serverSettings.postgresDbConnection' file.")), true, _httpresponcecode.DBConnection_error);
         return;
-    }
-    var client = new pg.Client(connection);
-    var jsonReturnResult = "";
-    client.connect(function (err) {
-        if (err) {
-            console.error('CONNECTION ERROR...');
-            var jsonString = JSON.stringify(err.message);
-            var jsonResult = JSON.parse(jsonString);
-            jsonReturnResult = jsonResult;
-            client.end();
-            console.log(jsonReturnResult);
-            callback(jsonReturnResult, true);
-        } else {
-            //console.log('CONNECTED...');
-            client.query(queryString, function (err, result) {
-                if (err) {
-                    // console.log("ERROR")
-                    // console.log(err.message);
-                    var jsonString = JSON.stringify(err.message);
-                    var jsonResult = JSON.parse(jsonString);
-                    jsonReturnResult = jsonResult;
-                    client.end();
-                    //console.log(jsonReturnResult);
-                    callback(jsonReturnResult, true);
-                } else {
-                    // console.log("SUCCESS")
-                    var jsonString = JSON.stringify(result.rows);
-                    var jsonResult = JSON.parse(jsonString);
-                    jsonReturnResult = jsonResult;
-
-                    client.end();
-                    ///console.log(jsonReturnResult);
-                    callback(jsonReturnResult, false);
-                }
-            });
-        }
-    });
+    };
 };
