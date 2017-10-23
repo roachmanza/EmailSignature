1378d5f5db945200dbcdc714f524cb889f6e34b0
roachmanza <roche.olivier@outlook.com>
2017-10-23 19:57:55 +0200

    Update dataaccess and removes config package

--- Api_Site/api/dataAccess/data/dataDelete.js
+++ Api_Site/api/dataAccess/data/dataDelete.js
@@ -1,34 +1,34 @@
 var pg = require('pg');
-var config = require('config');
 var _httpresponcecode = require('../../constants/httpresponcecodes');
-
+var dbconnections = require('../../../config/dbconnections');
 module.exports = function (queryString, callback) {
     var connection;
     var jsonReturnResult = "";
-    if (config.has('serverSettings.postgresDbConnection')) {
-        connection = config.get('serverSettings.postgresDbConnection');
-        var client = new pg.Client(connection);
-        client.connect(function (err) {
-            if (err) {
-                jsonReturnResult = JSON.parse(JSON.stringify(err.message));;
-                client.end();
-                callback(jsonReturnResult, true, _httpresponcecode.DBConnection_error); //Throws a 503
-            } else {
-                client.query(queryString, function (err, result) {
-                    if (err) {
-                        jsonReturnResult = JSON.parse(JSON.stringify(err.message));
-                        client.end();
-                        callback(jsonReturnResult, true, _httpresponcecode.Sql_error); //Throws a 400
-                    } else {
-                        jsonReturnResult = JSON.parse(JSON.stringify(result.rows));
-                        client.end();
-                        callback(jsonReturnResult, false, _httpresponcecode.Ok); //Throws a 200
-                    }
-                });
-            }
-        });
-    } else {
-        callback(JSON.parse(JSON.stringify("Connection error, could not find the 'serverSettings.postgresDbConnection' file.")), true, _httpresponcecode.DBConnection_error);
-        return;
-    };
+    var username = dbconnections.DBConnection_user;
+    var password = dbconnections.DBConnection_password;
+    var host = dbconnections.DBConnection_host;
+    var port = dbconnections.DBConnection_port;
+    var database = dbconnections.DBConnection_database;
+    var connection = "postgres://" + username + ":" + password + "@" + host + ":" + port + "/" + database;
+    var client = new pg.Client(connection);
+    client.connect(function (err) {
+        if (err) {
+            jsonReturnResult = JSON.parse(JSON.stringify(err.message));;
+            client.end();
+            callback(jsonReturnResult, true, _httpresponcecode.DBConnection_error); //Throws a 503
+        } else {
+            client.query(queryString, function (err, result) {
+                if (err) {
+                    jsonReturnResult = JSON.parse(JSON.stringify(err.message));
+                    client.end();
+                    callback(jsonReturnResult, true, _httpresponcecode.Sql_error); //Throws a 400
+                } else {
+                    jsonReturnResult = JSON.parse(JSON.stringify(result.rows));
+                    client.end();
+                    callback(jsonReturnResult, false, _httpresponcecode.Ok); //Throws a 200
+                }
+            });
+        }
+    });
+
 };
---
+++ Api_Site/config/dbconnections.js
@@ -0,0 +1,6 @@
+
+  exports.DBConnection_host = "localhost";
+  exports.DBConnection_database =  "MailEnhancement_Dev";
+  exports.DBConnection_port = 5432;
+  exports.DBConnection_user = "MailEnhancementUser";
+  exports.DBConnection_password = "MailEnhancementUser";
