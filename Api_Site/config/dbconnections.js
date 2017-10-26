d533e5f83ae4fdbb12486aff81b2963636ec79b1
roachmanza <roche.olivier@outlook.com>
2017-10-26 14:48:21 +0200

    Removed API folder to do a clean commit

--- Api_Site/config/dbconnections.js
+++
@@ -1,5 +0,0 @@
-  exports.DBConnection_host = "devtest.mmiholdings.co.za";
-  exports.DBConnection_database =  "MailEnhancement_Dev";
-  exports.DBConnection_port = 5432;
-  exports.DBConnection_user = "MailEnhancementUser";
-  exports.DBConnection_password = "MailEnhancementUser";
