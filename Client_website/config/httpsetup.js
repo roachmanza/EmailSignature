8cd7f289caa4f3a0a07cc07b30d1af867e044df0
Roche Olivier <ROlivier@mmiholdings.co.za>
2017-10-26 12:08:12 +0200

    Sync

---
+++ Client_website/config/httpsetup.js
@@ -0,0 +1,9 @@
+
+exports.http_port = 4010;
+exports.cors_allow_origin = "*";
+exports.cors_allow_methods = "GET,PUT,POST,DELETE";
+exports.cors_allow_headers = "Content-Type,MMI-Authorization-Claims";
+
+
+
+exports.http_env = "DEV";
